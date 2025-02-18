190760b24ce2788eb2388b4c0b8af519c0de2860
Heykrishna <heykrishna35@gmail.com>
2025-02-18 22:56:54 +0530

    Updating name

---
+++ 63_Video_Arrays/loops.js
@@ -0,0 +1,24 @@
+
+let a = [1, 3, 5, 6, 88]
+
+for(let i=0; i<a.length; i++){
+    const[element] = a[i];
+    console.log(element)
+}
+
+
+
+// a.forEach((value, index, array)=>{ 
+//     console.log(value, index, array); 
+// })
+
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+console.log('obj', obj);
+
+

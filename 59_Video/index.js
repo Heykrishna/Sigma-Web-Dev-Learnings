a838a9681dec6fd9e1ec3ee9c23768516bb6b7b6
Heykrishna <heykrishna35@gmail.com>
2025-02-18 07:20:14 +0530

    Pushing code

---
+++ 59_Video/index.js
@@ -0,0 +1,12 @@
+// Faulty calculator 
+
+// This faulty calculator takes two input from the user
+// It performs wrong operation as follows 
+// It performs wrong operation 10% of the time
+
+// + ---> - 
+// * ---> + 
+// - ---> /
+// / ---> ** 
+
+

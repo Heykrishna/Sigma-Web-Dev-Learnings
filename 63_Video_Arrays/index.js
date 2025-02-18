8fb2e5d2bbf1ef88211c02909af61e87b87921f4
Heykrishna <heykrishna35@gmail.com>
2025-02-18 09:08:01 +0530

    Pushing code

---
+++ 63_Video_Arrays/index.js
@@ -0,0 +1,17 @@
+// JavaScript Arrays
+
+
+// Collection of multiple data types 
+// Strings are immutable, but arrays are mutable 
+
+
+let arr = [1, 2, 3, 4, 5]
+
+arr[0] = 10
+
+console.log(arr)
+console.log(arr.length)
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
